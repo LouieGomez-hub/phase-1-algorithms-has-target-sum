b80e8d1a7752fece7374b8e22ac715716b2a4279
louie_gomez <kinggomez027.lg@gmail.com>
2022-05-02 20:47:31 -0400

    commit tree 375b06f0b352f6d72faab1cbb812b3e326ae6be5

--- index.js
+++ index.js
@@ -1,22 +1,54 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
-}
+  for(let i = 0; i < array.length; i++){
+    const complement = target - array[i];
+    for(let j = i + 1; j < array.length; j++){
+      if(array[j] === complement)
+        return true;
+      }
+    }
+    return false;
+  }
 
 /* 
-  Write the Big O time complexity of your function here
+  n steps (depending on the length of the input array)
+  n * n steps (nested loop!)
+  1 step
+  (quadratic)
 */
 
 /* 
-  Add your pseudocode here
+  iterate through each number in the array
+  identify a complementary number that adds to our target
+  iterate through the rest of the array
+  if any of the remaining numbers is the complement
+    return true
+  if we reach the end of the array
+    return false
 */
 
 /*
-  Add written explanation of your solution here
+  make a hasTargetSum function that checks 
+  if two numbers in an array add up to some target number
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+  
+  // Negative numbers?
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([-7, 10, 4, 8], 3));
+
+  console.log("");
+  // Multiple pairs?
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([1, 2, 3, 4], 5));
+
+  console.log("");
+  // Single numbers?
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([4], 4));
+
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
